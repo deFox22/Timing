fe922dd2ce7eb4659d8da5eb09c14b184a44192e
Cameron de Vos <cameron2@outlook.com>
2014-11-25 22:44:53 -0800

    Add TypeError example

---
+++ Students/SteveDooer/session01/break_me.py
@@ -0,0 +1,22 @@
+#Explore Errors
+
+#Create a NameError
+#nameerrors value is not referenced before being called
+
+def createNameError():
+	return nameerrors
+
+#Create a TypeError
+#Cannot add int and str together beacause they are different types
+def createTypeError():
+	a = 5
+	b = "you"
+	return a + b
+
+
+
+
+
+
+# createNameError()
+createTypeError()
