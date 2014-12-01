071e9e50ad6ebc4ecb265f91eb955feabe22f180
Cameron de Vos <cameron2@outlook.com>
2014-11-30 23:04:35 -0800

    Add testing for ackermann function

--- Students/SteveDooer/session02/ack.py
+++ Students/SteveDooer/session02/ack.py
@@ -12,3 +12,36 @@ def ack(m, n):
         return ack(m-1, 1)
     else:
         return ack(m-1, ack(m, n-1))
+
+# Test to verify that all results below max recursion depth
+# return a value that matches expected result.  
+
+if __name__ == '__main__':
+    
+    ack_list = [
+    (0,0,1),
+    (0,1,2),
+    (0,2,3),
+    (0,3,4),
+    (0,4,5),
+    (1,0,2),
+    (1,1,3),
+    (1,2,4),
+    (1,3,5),
+    (1,4,6),
+    (2,0,3),
+    (2,1,5),
+    (2,2,7),
+    (2,3,9),
+    (2,4,11),
+    (3,0,5),
+    (3,1,13),
+    (3,2,29),
+    (3,3,61),
+    (3,4,125)
+    ]
+    
+    for m, n, result in ack_list:
+        assert ack(m, n) == result
+
+    print (u"All Tests Pass")
